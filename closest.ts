47aeb0778fe3be3f2a45d2d5dfcf454cc9215029
Lumina <erik04.nivala22@gmail.com>
2024-09-04 21:51:45 -0500

    Solved problem

--- closest.ts
+++ closest.ts
@@ -1,6 +1,24 @@
 export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
+  const sorted = nums.sort((a, b) => a - b);
+  let closestSum = Infinity;
+
+  for (let i = 0; i < sorted.length - 2; i++) {
+    let left = i + 1;
+    let right = sorted.length - 1;
+
+    while (left < right) {
+      let currentSum = sorted[i] + sorted[left] + sorted[right];
+      if (currentSum === target) return currentSum;
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target))
+        closestSum = currentSum;
+
+      if (currentSum < target) left++;
+      else right--;
+    }
+  }
+
+  return closestSum;
 }
 
-module.exports = { closest }
+module.exports = { closest };
